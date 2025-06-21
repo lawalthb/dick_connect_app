e0b6f6a31259ad6db67ebf3c46e3f9e844babb50
Ogheneochuko Duvwarovwo <ochukodick@gmail.com>
2025-06-21 17:36:10 +0100

    Updated profile image and user profile

--- src/components/Connecting/ProfileCard.jsx
+++ src/components/Connecting/ProfileCard.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import NigerieFlag from '@/Images/NigerieFlag.png';
 import Button from '../Button';
+import { useState } from 'react';
 
 const ProfileCard = ({
   profile,
@@ -9,73 +10,97 @@ const ProfileCard = ({
   userCircles,
   handleViewProfile,
 }) => {
+  const [isImageClicked, setIsImageClicked] = useState(false);
+
+  const handleImageClick = () => {
+    console.log('Image clicked');
+    setIsImageClicked((prev) => !prev);
+  };
   return (
-    <div className=" rounded-[30px] mt-10 w-full mx-auto px-2 sm:px-4 shadow-md transition-shadow duration-300 hover:shadow-lg">
+    <div
+      onClick={handleImageClick}
+      className="rounded-[30px] mt-10 w-full mx-auto px-2 sm:px-4 shadow-md transition-shadow duration-300 hover:shadow-lg"
+    >
       <div className="relative w-full rounded-[30px] overflow-hidden">
         <Image
           src={profile.image}
           alt={profile.name}
           width={805}
           height={783}
-          className="object-cover w-full h-[600px] lg:h-auto rounded-[30px]"
+          className="object-cover w-full h-[600px] md:h-[1000px] rounded-[30px]"
         />
-        <div className=" absolute bottom-20 left-52 sm:left-60 -translate-x-1/2 w-[387px] text-white leading-5">
-          <div className="flex items-center gap-2">
-            <h3 className="font-bold text-[24px] lg:text-[30px]">
-              {profile.name}
-            </h3>
-            <Image src={NigerieFlag} alt="Country" width={20} height={10} />
-          </div>
-          <p className="font-normal text-[20px] md:text-[24px] mt-2 lg:mt-4">
-            {profile.profession}
-          </p>
-          <p className="font-bold text-[16px] md:text-[20px] mt-3 lg:mt-7">
-            {`Connected: ${profile.connections}`}
-          </p>
 
-          <div className="flex gap-2 sm:gap-4 mb-8 mt-4 items-center justify-start flex-wrap">
-            {userConnections.map((option, index) => (
-              <div
-                key={index}
-                onClick={() => handleOptionClick(option.id)}
-                className="max-h-[40px] lg:max-h-[60px]"
-              >
-                <Image
-                  src={option.image}
-                  alt="User"
-                  width={60}
-                  height={60}
-                  className="cursor-pointer rounded-full object-cover aspect-square"
+        {!isImageClicked && (
+          <>
+            <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/40 to-transparent z-10" />
+
+            <div className="absolute inset-0 z-20 flex flex-col justify-end px-4 sm:px-10 pb-20 text-white">
+              <div className="max-w-[387px]">
+                <div className="flex items-center gap-2">
+                  <h3 className="font-bold text-[24px] md:text-[30px]">
+                    {profile.name}
+                  </h3>
+                  <Image
+                    src={NigerieFlag}
+                    alt="Country"
+                    width={20}
+                    height={10}
+                  />
+                </div>
+                <p className="font-normal text-[20px] md:text-[24px] mt-2 lg:mt-4">
+                  {profile.profession}
+                </p>
+                <p className="font-bold text-[16px] md:text-[20px] mt-3 lg:mt-7">
+                  {`Connected: ${profile.connections}`}
+                </p>
+
+                <div className="flex gap-2 sm:gap-4 mb-8 mt-4 items-center justify-start flex-wrap">
+                  {userConnections.map((option, index) => (
+                    <div
+                      key={index}
+                      onClick={() => handleOptionClick(option.id)}
+                      className="max-h-[40px] lg:max-h-[60px]"
+                    >
+                      <Image
+                        src={option.image}
+                        alt="User"
+                        width={60}
+                        height={60}
+                        className="cursor-pointer rounded-full object-cover aspect-square"
+                      />
+                    </div>
+                  ))}
+                </div>
+
+                <div className="flex flex-wrap gap-2 sm:gap-4 mt-4 items-center justify-start">
+                  {userCircles.map((option, index) => (
+                    <div
+                      key={index}
+                      className="bg-[#A20030] text-white rounded-[50px] px-4 sm:px-5 py-1.5 sm:py-2 shadow-md text-sm sm:text-base leading-6 font-medium whitespace-nowrap"
+                    >
+                      {`#${option.circle}`}
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              <div className="mt-6 flex gap-x-4 max-w-[650px]">
+                <Button
+                  label="Show Circle"
+                  variant="outlined"
+                  btnclass="w-full h-10 bg-white whitespace-nowrap sm:px-16"
+                  btnstyle="rounded"
+                />
+                <Button
+                  label="View Profile"
+                  onClick={handleViewProfile}
+                  btnclass="w-full h-10 whitespace-nowrap sm:px-16"
+                  btnstyle="rounded"
                 />
               </div>
-            ))}
-          </div>
-          <div className="flex flex-wrap gap-2 sm:gap-4 mt-4 items-center justify-start">
-            {userCircles.map((option, index) => (
-              <div
-                key={index}
-                className="bg-[#A20030] text-white rounded-[50px] px-4 sm:px-5 py-1.5 sm:py-2 shadow-md text-sm sm:text-base leading-6 font-medium whitespace-nowrap"
-              >
-                {`#${option.circle}`}
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="absolute bottom-5 left-1 sm:left-10 flex gap-x-5 max-w-[650px] items-center justify-center">
-          <Button
-            label="Show Circle"
-            variant="outlined"
-            // onClick={handleBackToPerformance}
-            btnclass="w-full h-10 bg-white whitespace-nowrap sm:px-16"
-            btnstyle="rounded"
-          />
-          <Button
-            label="View Profile"
-            onClick={handleViewProfile}
-            btnclass="w-full h-10 whitespace-nowrap sm:px-16"
-            btnstyle="rounded"
-          />
-        </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
--- src/components/Connecting/UserProfile.jsx
+++ src/components/Connecting/UserProfile.jsx
@@ -49,27 +49,43 @@ const UserProfile = () => {
 
   return (
     <div>
-      <Swiper
-        style={{ '--swiper-pagination-color': '#A20030' }}
-        modules={[EffectFlip, Autoplay, Pagination]}
-        pagination={{ clickable: true }}
-        effect="flip"
-        grabCursor={true}
-        loop={true}
-        className="w-full max-w-[805px] mx-auto rounded-[30px]"
-      >
-        {[Daniella, Daniella, Daniella].map((img, index) => (
-          <SwiperSlide key={index}>
-            <Image
-              src={img}
-              alt={`Profile Image ${index + 1}`}
-              width={805}
-              height={783}
-              className="object-cover w-full h-[400px] lg:h-auto rounded-[30px]"
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className="relative w-full max-w-[805px] mx-auto pb-12">
+        <Swiper
+          modules={[EffectFlip, Autoplay, Pagination]}
+          effect="flip"
+          grabCursor={true}
+          loop={true}
+          pagination={{ clickable: true }}
+          style={{
+            '--swiper-pagination-color': '#A20030',
+            '--swiper-pagination-bottom': '-30px',
+          }}
+          className="rounded-[30px]"
+        >
+          {[Daniella, Daniella, Daniella].map((img, index) => (
+            <SwiperSlide key={index}>
+              <Image
+                src={img}
+                alt={`Profile Image ${index + 1}`}
+                width={805}
+                height={783}
+                className="object-cover w-full h-[400px] lg:h-auto rounded-[30px]"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        {/* <style jsx global>{`
+          .swiper-pagination {
+            border: 1px solid #a20030;
+            padding: 8px 6px;
+            border-radius: 20px;
+            display: inline-flex;
+            max-width: 100%;
+            justify-content: center;
+            align-items: center;
+          }
+        `}</style> */}
+      </div>
       <ProfileDetail />
       <ConnectCategiries connectOptions={connectOptions} isProfile={true} />
       <ConnectStory extraStyle="text-[24px]" />
